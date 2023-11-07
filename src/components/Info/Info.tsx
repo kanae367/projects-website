0a9b8d293e84c657e4d18a368d23875c0f021bda
”Kodoku” <kodokunawa@gmail.com>
2023-11-07 18:12:12 +0300

    Изменена структура файла icons.json, взаимодействие с ним в коде

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -3,38 +3,15 @@ import { Navigation, A11y, Mousewheel } from 'swiper/modules';
 import iconsSources from '../../icons.json';
 import style from './info.module.scss';
 
-const technologiesData: {
-    src: string,
-    alt: string
-}[] = [
-    {
-        src: 'html.svg',
-        alt: 'HTML 5'
-    },
-    {
-        src: 'css3.svg',
-        alt: 'CSS 3'
-    },
-    {
-        src: 'jquery.svg',
-        alt: 'JQuery'
-    },
-    {
-        src: 'js.svg',
-        alt: 'JavaScript'
-    },
-    {
-        src: 'figma.svg',
-        alt: 'Figma'
-    },
-]
-
 const Info = ({name, text, technologies}: {
     name: string,
     text: string,
     technologies: string[]
 }) => {
-    const srcArray = technologies.map(item => iconsSources[item])
+    const srcArray: {
+        src: string;
+        alt: string;
+    }[] = technologies.map(item => iconsSources[iconsSources.findIndex(obj => obj.name === item)].data);
     const swiperSlides = srcArray.map(item => <SwiperSlide key={item.src} className={style.slide}><img className={style.image} src={`/icons/${item.src}`} alt={item.alt} /></SwiperSlide>)
 
     return(
