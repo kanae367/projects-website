24ba99a21bec75229553602bad67eab7d756cd12
”Kodoku” <kodokunawa@gmail.com>
2024-01-04 23:40:15 +0300

    Удалены ненужные выводы в консоль

--- src/preloadImage.ts
+++ src/preloadImage.ts
@@ -1,5 +1,4 @@
 export const preloadImage = (imgSrc: string) => {
     const image = new Image();
     image.src = imgSrc;
-    image.addEventListener('load', () => console.log('image loaded, src: ' + imgSrc));
 } 
